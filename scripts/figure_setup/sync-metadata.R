bd158614dab1bea43d9814fa7edd80db8611b6a6
Ally Hawkins <54039191+allyhawkins@users.noreply.github.com>
2024-01-04 13:43:40 -0600

    pull out syncing metadata into a separate script

---
+++ scripts/figure_setup/sync-metadata.R
@@ -0,0 +1,22 @@
+# This script is used to download and save metadata from S3 to a local directory
+# All files will be saved in a folder named `s3_files`
+
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+
+# path to metadata stored on S3
+sample_metadata_s3 <- 's3://ccdl-scpca-data/sample_info/scpca-sample-metadata.tsv'
+library_metadata_s3 <- 's3://ccdl-scpca-data/sample_info/scpca-library-metadata.tsv'
+
+# create a directory to store S3 files 
+local_s3_dir <- file.path(root_dir, "s3_files")
+fs::dir_create(local_s3_dir)
+
+# Copy sample metadata 
+local_sample_metadata <- file.path(local_s3_dir, "scpca-sample-metadata.tsv")
+sync_call <- paste('aws s3 cp', sample_metadata_s3, local_s3_dir, sep = " ")
+system(sync_call)
+
+# copy library metadata
+local_library_metadata <- file.path(local_s3_dir, "scpca-library-metadata.tsv")
+sync_call <- paste('aws s3 cp', library_metadata_s3, local_s3_dir, sep = " ")
+system(sync_call)
