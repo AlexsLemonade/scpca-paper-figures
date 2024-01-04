6f655cc4992f0bb6915a84e41dfed365e5df6fa0
Ally Hawkins <54039191+allyhawkins@users.noreply.github.com>
2024-01-04 13:43:53 -0600

    make legend and patterns match

--- scripts/fig1-sample_disease_barchart.R
+++ scripts/fig1-sample_disease_barchart.R
@@ -9,18 +9,14 @@ library(ggplot2)
 
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 
-# path to sample metadata stored on S3
-sample_metadata_s3 <- 's3://ccdl-scpca-data/sample_info/scpca-sample-metadata.tsv'
-
-# create a directory to store S3 files 
-local_s3_dir <- file.path(root_dir, "s3_files")
-fs::dir_create(local_s3_dir)
-
 # all metadata files 
 sample_info_dir <- file.path(root_dir, "sample-info")
 project_whitelist_file <- file.path(sample_info_dir, "project-whitelist.txt")
 diagnosis_groupings_file <- file.path(sample_info_dir, "diagnosis-groupings.tsv")
 disease_timing_file <- file.path(sample_info_dir, "disease-timing.tsv")
+
+# path to sample metadata 
+sample_metadata_file <- file.path(root_dir, "s3_files", "scpca-sample-metadata.tsv")
 
 # color palette
 diagnosis_group_palette <- file.path(root_dir, "palettes", "diagnosis-group-palette.tsv")
@@ -37,13 +33,8 @@ diagnosis_groupings_df <- readr::read_tsv(diagnosis_groupings_file) |>
   dplyr::select(submitted_diagnosis, diagnosis_group)
 disease_timing_df <- readr::read_tsv(disease_timing_file)
 
-# Copy sample metadata 
-local_sample_metadata <- file.path(local_s3_dir, "scpca-sample-metadata.tsv")
-sync_call <- paste('aws s3 cp', sample_metadata_s3, local_s3_dir, sep = " ")
-system(sync_call)
-
 # read in sample metadata and filter to only projects in whitelist
-sample_metadata_df <- readr::read_tsv(local_sample_metadata) |> 
+sample_metadata_df <- readr::read_tsv(sample_metadata_file) |> 
   dplyr::filter(scpca_project_id %in% project_whitelist)
 
 # Join sample metadata with diagnosis grouping and disease timing 
@@ -82,33 +73,36 @@ diagnosis_colors <- diagnosis_group_palette_df$color |>
 # diagnosis is on the y axis and number of samples on the x axis
 # bars are patterned based on the disease timing group 
 diagnosis_plot <- ggplot(plot_df, aes(y = diagnosis,  fill = diagnosis_group)) +
-  ggpattern::geom_bar_pattern(aes(pattern = standardized_disease_timing), 
+  ggpattern::geom_bar_pattern(aes(pattern = forcats::fct_rev(standardized_disease_timing)), 
                               color = "black",
                               pattern_color = "black",
                               pattern_fill = "black",
                               pattern_density = 0.04,
-                              pattern_spacing = 0.02) +
+                              pattern_spacing = 0.02)+
   facet_wrap(facets = "diagnosis_group", scales = "free") +
   # add label for the number of samples in each diagnosis group 
-  geom_text(aes(label = after_stat(count)), stat = "count", hjust = -0.25, size = 3) +
+  geom_text(aes(label = after_stat(count)), stat = "count", hjust = -0.25, size = 4) +
   # manually set patterns 
   ggpattern::scale_pattern_manual(values = c(
-    "Initial diagnosis" = 'circle', 
+    "Initial diagnosis" = 'none', 
     "Post-mortem" = 'stripe', 
     "Progressive" = 'crosshatch', 
-    "Recurrence" = 'none',
+    "Recurrence" = 'circle',
     "Unknown" = 'wave'), 
     drop = FALSE) +
   # set colors to use palette for diagnosis groups
   scale_fill_manual(values = diagnosis_colors) +
   # make legend black and white 
   guides(fill = "none", 
-         pattern = guide_legend(override.aes = list(fill = "white"), title.position = "top", title.hjust = 0.5)) + 
+         pattern = guide_legend(override.aes = list(fill = "white"), 
+                                title.position = "top", 
+                                title.hjust = 0.5,
+                                reverse = TRUE)) + 
   labs(pattern = "Disease timing",
        x = "Number of samples",
        y = "Diagnosis") + 
   theme_classic() + 
-  theme(text = element_text(size = 10),
+  theme(text = element_text(size = 14),
         legend.position = "top",
         legend.key.size = unit(1, 'cm'),
         plot.margin = margin(1, 1, 1, 1, 'cm')
@@ -118,4 +112,4 @@ diagnosis_plot <- ggplot(plot_df, aes(y = diagnosis,  fill = diagnosis_group)) +
   coord_cartesian(clip = "off")
 
 # save plot 
-ggsave(output_plot_file, plot = diagnosis_plot)
+ggsave(output_plot_file, plot = diagnosis_plot, width = 15, height = 10)
