a0db41343f077e282d7474c51948c363ab066b6e
Ally Hawkins <54039191+allyhawkins@users.noreply.github.com>
2024-04-04 09:58:00 -0500

    make sure multiplexed kits don't count more than once

--- scripts/TableS1_modality-summary.R
+++ scripts/TableS1_modality-summary.R
@@ -31,7 +31,7 @@ project_whitelist <- readLines(project_whitelist_file)
 
 # read in project metadata files and create sample whitelist 
 project_metadata_files <- readr::read_tsv(project_metadata_file) |> 
-  dplyr::filter(scpca_project_id %in% projects) |> 
+  dplyr::filter(scpca_project_id %in% project_whitelist) |> 
   dplyr::pull(metadata_file)
 # get full file paths to each project metadata file
 project_metadata_files <- file.path(s3_file_dir, "project-metadata", project_metadata_files)
@@ -50,10 +50,9 @@ sample_whitelist <- project_metadata_files |>
 # read in groupings
 diagnosis_groupings_df <- readr::read_tsv(diagnosis_groupings_file)
 
-# read in library metadata and filter to included projects and samples
+# read in library metadata and filter to included projects
 library_metadata_df <- readr::read_tsv(library_metadata_file) |> 
-  dplyr::filter(scpca_project_id %in% project_whitelist,
-                scpca_sample_id %in% sample_whitelist)
+  dplyr::filter(scpca_project_id %in% project_whitelist)
 
 # read in sample metadata and filter to included projects and samples
 sample_metadata_df <- readr::read_tsv(sample_metadata_file) |> 
@@ -69,7 +68,9 @@ demuxed_metadata_df <- library_metadata_df |>
   # make sure we have one row per library/ sample combination
   # this specifically separates sample IDs for multiplex libraries for easier joining to sample metadata
   dplyr::mutate(scpca_sample_id = stringr::str_split(scpca_sample_id, ";")) |> 
-  tidyr::unnest(scpca_sample_id)
+  tidyr::unnest(scpca_sample_id) |> 
+  # now that sample ids are separated, filter to whitelist 
+  dplyr::filter(scpca_sample_id %in% sample_whitelist)
   
 # create a total count of the number of libraries per project per diagnosis group
 total_library_count <- demuxed_metadata_df |>
@@ -89,7 +90,7 @@ total_sample_count <- demuxed_metadata_df |>
 modality_counts_df <- demuxed_metadata_df |> 
   dplyr::mutate(
     # make a new column that summarizes all modality info
-    # options are cell, nucleus, bulk, spatial, cite, multiplexed, and 10X kits
+    # options are cell, nucleus, bulk, spatial, cite, and multiplexed
     # the assigned values will eventually be the column names used in the output
     modality = dplyr::case_when(
       seq_unit == "bulk" ~ "Bulk RNA",
@@ -98,19 +99,12 @@ modality_counts_df <- demuxed_metadata_df |>
       stringr::str_detect(technology,"cellhash") ~ "With cell hashing",
       seq_unit == "cell" ~ "Single-cell",
       seq_unit == "nucleus" ~ "Single-nucleus"
-    ),
-    # create a separate column with the kit used for each sample
-    kit_type = dplyr::case_when(
-      # account for both 10Xv2 3' and 5'
-      technology %in% c("10Xv2", "10Xv2_5prime") ~ "10Xv2",
-      technology == "10Xv3" ~ "10Xv3",
-      technology == "10Xv3.1" ~ "10Xv3.1"
     )
   ) |> 
   # join with sample metadata to add in diagnosis groupings
   dplyr::left_join(sample_metadata_df) |>
   dplyr::select(
-    scpca_project_id, scpca_library_id, modality, kit_type, diagnosis, diagnosis_group
+    scpca_project_id, scpca_library_id, modality, diagnosis, diagnosis_group
   ) 
 
 # determine full diagnoses for each project
@@ -120,8 +114,17 @@ full_diagnoses <- modality_counts_df |>
   dplyr::summarize(diagnosis = paste(unique(diagnosis), collapse = ";"))
 
 # get total numbers of libraries for each 10X kit for each project 
-kit_counts <- modality_counts_df |> 
-  dplyr::select(scpca_project_id, scpca_library_id, diagnosis_group, kit_type) |> 
+kit_counts <- demuxed_metadata_df |> 
+  dplyr::left_join(sample_metadata_df) |>
+  dplyr::select(scpca_project_id, scpca_library_id, diagnosis_group, technology) |> 
+  dplyr::distinct() |> 
+  dplyr::mutate(    # create a separate column with the kit used for each sample
+    kit_type = dplyr::case_when(
+      # account for both 10Xv2 3' and 5'
+      technology %in% c("10Xv2", "10Xv2_5prime") ~ "10Xv2",
+      technology == "10Xv3" ~ "10Xv3",
+      technology == "10Xv3.1" ~ "10Xv3.1"
+    )) |> 
   # remove any NA, these are cell hash, bulk, ST, and CITE
   tidyr::drop_na(kit_type) |> 
   # count each combination of project, diagnosis group and kit
@@ -129,7 +132,7 @@ kit_counts <- modality_counts_df |>
   tidyr::pivot_wider(names_from = kit_type, values_from = n, values_fill = 0)
 
 summarized_counts_df <- modality_counts_df |>
-  dplyr::select(scpca_project_id, scpca_library_id, diagnosis_group, modality, kit_type) |>
+  dplyr::select(scpca_project_id, scpca_library_id, diagnosis_group, modality) |>
   dplyr::distinct() |>
   # count each combination of diagnosis group and modality
   dplyr::count(scpca_project_id, diagnosis_group, modality) |>
