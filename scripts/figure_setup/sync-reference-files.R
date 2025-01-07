0e721a88f1c0241628f1ffbd93f8268522082c15
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-01-07 12:28:14 -0500

    dont need local file definitions

--- scripts/figure_setup/sync-reference-files.R
+++ scripts/figure_setup/sync-reference-files.R
@@ -13,20 +13,17 @@ annotation_s3 <- "s3://scpca-references/homo_sapiens/ensembl-104/annotation"
 
 # sync mito file -----
 mito_s3 <- file.path(annotation_s3, "Homo_sapiens.GRCh38.104.mitogenes.txt")
-local_mito_file <- file.path(local_s3_dir, "Homo_sapiens.GRCh38.104.mitogenes.txt")
-sync_call <- glue::glue("aws s3 cp '{mito_s3}' '{local_mito_file}'")
+sync_call <- glue::glue("aws s3 cp '{mito_s3}' '{local_s3_dir}'")
 system(sync_call)
 
 # sync t2g file -----
 t2g_s3 <- file.path(annotation_s3, "Homo_sapiens.GRCh38.104.spliced_cdna.tx2gene.tsv")
-local_t2g_file <- file.path(local_s3_dir, "Homo_sapiens.GRCh38.104.spliced_cdna.tx2gene.tsv")
-sync_call <- glue::glue("aws s3 cp '{t2g_s3}' '{local_t2g_file}'")
+sync_call <- glue::glue("aws s3 cp '{t2g_s3}' '{local_s3_dir}'")
 system(sync_call)
 
 
 # sync SingleR models  ---------
 singler_s3_dir <- "s3://scpca-references/celltype/singler_models"
 singler_local_dir <- here::here("s3_files", "reference_files", "singler_models")
-
 sync_call <- glue::glue("aws s3 cp '{singler_s3_dir}' '{singler_local_dir}' --recursive")
 system(sync_call)
