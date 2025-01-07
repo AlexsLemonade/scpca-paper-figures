a7544050b77f985bbdd3b36968f909212adbb834
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-01-07 10:07:39 -0500

    update and rename sync script to also prepare and export a table for id mapping

--- analysis/bulk-deconvolution/scripts/prepare-data-files.R
+++ analysis/bulk-deconvolution/scripts/prepare-data-files.R
@@ -1,10 +1,30 @@
-# This script is used to download data needed for bulk solid tumor deconvolution analysis
-# All files will be saved in `../data/salmon-quant-files/<project id>/<sample id>/<library id>`
-# 
-# This script assumes that the ../../s3_files/scpca-<sample/library>-metadata.tsv files present. These files can be obtained with ../../scripts/figure_setup/sync-metadata.R.
+# This script prepares several data files for use in the bulk deconvolution anaylsis:
+# - Exports a table for ensembl/gene symbol id mapping to `../data/reference/ensembl_symbol.tsv`, based on an ScPCA SCE 
+# - Syncs all `quant.sf` files from S3 needed for bulk solid tumor deconvolution analysis to `../data/salmon-quant-files/<project id>/<sample id>/<library id>` 
+#
+# This script assumes that the following files are present in ../../s3_files/:
+# - reference_files/scpca-sample-metadata.tsv
+# - reference_files/scpca-library-metadata.tsv
+# - SCPCS000001/SCPCL000001_processed.rds
+# These files can be obtained with ../../scripts/figure_setup/sync-metadata.R and ../../scripts/figure_setup/sync-data-files.R
 
 renv::load()
+library(optparse)
 
+
+# Parse options --------
+option_list <- list(
+  make_option(
+    "--scpca_sce_file",
+    type = "character",
+    default = here::here("s3_files", "SCPCS000001", "SCPCL000001_processed.rds"),
+    help = "Path to an SCE file used to obtain mappings for converting ensembl ids to gene symbols."
+  )
+)
+opts <- parse_args(OptionParser(option_list = option_list))
+stopifnot("The SCE file to use for id mapping could not be found." = file.exists(opts$scpca_sce_file))
+
+  
 # Define files and directories ---------
 library_metadata_file <- here::here("s3_files", "scpca-library-metadata.tsv")
 sample_metadata_file <- here::here("s3_files", "scpca-sample-metadata.tsv")
@@ -12,6 +32,19 @@ bulk_dir <- here::here("analysis", "bulk-deconvolution")
 s3_path <- "s3://nextflow-ccdl-results/scpca-prod/checkpoints/salmon" #/library_id/quant.sf
 data_path <- file.path(bulk_dir, "data", "salmon-quant-files")
 fs::dir_create(data_path)
+
+map_tsv_path <- file.path(bulk_dir, "data", "reference", "ensembl_symbol.tsv") 
+fs::dir_create(dirname(map_tsv_path))
+
+# First, export the TSV file for id mapping ------------------
+sce <- readRDS(opts$scpca_sce_file)
+map_table <- data.frame(
+  ensembl_id = SingleCellExperiment::rowData(sce)$gene_ids,
+  gene_symbol = SingleCellExperiment::rowData(sce)$gene_symbol
+)
+readr::write_tsv(map_table, map_tsv_path)
+
+# Second, obtain the quant.sf files ------------------
 
 # Read in the metadata files and parse it for bulk samples of interest ---------
 # Parsing code adapted from: https://github.com/AlexsLemonade/scpca-paper-figures/issues/98#issuecomment-2573164429
