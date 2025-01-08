d02cd7c7ce65227c04ab8e707bb33334b5a72a2a
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-01-08 15:17:08 -0500

    update arguments to take input and output files

--- analysis/bulk-deconvolution/scripts/run-quantiseq.R
+++ analysis/bulk-deconvolution/scripts/run-quantiseq.R
@@ -8,40 +8,30 @@ library(quantiseqr)
 # Parse options --------
 option_list <- list(
   make_option(
-    "--project_id",
+    "--input_file",
     type = "character",
-    default = "SCPCP000001",
-    help = "ScPCA project id to run quanTIseq on."
+    help = "Input RDS file with a TPM matrix."
   ),
   make_option(
-    "--input_dir",
+    "--output_file",
     type = "character",
-    default = here::here("analysis", "bulk-deconvolution", "data", "tpm"),
-    help = "Input directory containing a project-specific TPM matrices saved as rds files named <project id>-tpm.rds."
-  ),
-  make_option(
-    "--output_dir",
-    type = "character",
-    help = "Output directory to quanTIseq inference TSV."
+    help = "Output TSV file to save quanTIseq inference to."
   )
 )
 opts <- parse_args(OptionParser(option_list = option_list))
 
 # Check inputs and define paths -------
 stopifnot(
-  "A project id in the format SCPCPXXXXXX must be provided to `project_id`." = !is.null(opts$project_id),
-  "An output directory must be specified with `output_dir`." = !is.null(opts$output_dir)
+  "A path to an input RDS file must be specified wit `input_file`." = !is.null(opts$input_file),
+  "A path to an output TSV must be specified with `output_file`." = !is.null(opts$output_file)
 )
 
-tpm_file <- file.path(opts$input_dir, glue::glue("{opts$project_id}-tpm.rds"))
-stopifnot("There is no TPM matrix for the given `project_id` in the provided `input_dir`." = file.exists(tpm_file))
+stopifnot("The provided `input_file` does not exist." = file.exists(opts$input_file))
 
-fs::dir_create(opts$output_dir)
-output_file <- file.path(opts$output_dir, glue::glue("{opts$project_id}-quantiseq.tsv"))
-
+fs::dir_create(dirname(opts$output_file))
 
 # Prepare input rds file -----------
-tpm_matrix <- readr::read_rds(tpm_file)
+tpm_matrix <- readr::read_rds(opts$input_file)
 
 # As determined in ../exploratory-notebooks/quantiseq-tumor-genes.Rmd, several gene names in the signature 
 #  are outdated. Here, we'll replace those gene symbols in our matrix row names with the gene symbols quanTIseq expects:
@@ -78,4 +68,4 @@ deconv_tidy_df <- deconv_df |>
   dplyr::rename(sample_id = Sample) 
 
 # Export to tsv -------
-readr::write_tsv(deconv_tidy_df, output_file)
+readr::write_tsv(deconv_tidy_df, opts$output_file)
