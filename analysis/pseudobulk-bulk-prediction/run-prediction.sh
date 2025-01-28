1dbd2707eef96c18334a31c4dfb80957b2af7dd6
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-01-28 16:23:28 -0500

    update run script

--- analysis/pseudobulk-bulk-prediction/run-prediction.sh
+++ analysis/pseudobulk-bulk-prediction/run-prediction.sh
@@ -11,26 +11,25 @@ cd "$basedir"
 # Define directories
 data_dir="data"
 script_dir="scripts"
-salmon_quant_dir="${data_dir}/salmon-quant-files"
-reference_dir=${data_dir}/reference # stores the id map file
+scpca_dir="${data_dir}/scpca_data"
 tpm_dir="${data_dir}/tpm"
 result_dir="results"
 
-mkdir -p $salmon_quant_dir
-mkdir -p $reference_dir
+mkdir -p $scpca_dir
+mkdir -p $tpm_dir
 mkdir -p $result_dir
 
-echo "preparing"
 # Step 0: Prepare sync quant.sf files if they do not exist
-Rscript ${script_dir}/prepare-data-files.R
+Rscript ${script_dir}/prepare-data-files.R \
+  --output_dir ${scpca_dir}
 
-echo "looping"
-for project_dir in $salmon_quant_dir/*; do
+for project_dir in $scpca_dir/*; do
     project_id=$(basename $project_dir)
 
     tpm_file="${tpm_dir}/${project_id}-tpm.tsv"
+
     # Step 1: Calculate TPM for each project
     Rscript ${script_dir}/calculate-tpm.R \
-        --input_dir "${salmon_quant_dir}/${project_id}" \
+        --input_dir "${scpca_dir}/${project_id}" \
         --output_file "${tpm_file}"
 done
