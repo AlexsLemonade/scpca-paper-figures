b425ae051b53e2947ccdd396f58f192f8b060c04
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-01-29 09:35:22 -0500

    add pseudobulk step

--- analysis/pseudobulk-bulk-prediction/run-prediction.sh
+++ analysis/pseudobulk-bulk-prediction/run-prediction.sh
@@ -13,10 +13,12 @@ data_dir="data"
 script_dir="scripts"
 scpca_dir="${data_dir}/scpca_data"
 tpm_dir="${data_dir}/tpm"
+pseudobulk_dir="${data_dir}/pseudobulk"
 result_dir="results"
 
 mkdir -p $scpca_dir
 mkdir -p $tpm_dir
+mkdir -p $pseudobulk_dir
 mkdir -p $result_dir
 
 # Step 0: Sync data files from S3
@@ -27,9 +29,19 @@ for project_dir in $scpca_dir/*; do
     project_id=$(basename $project_dir)
 
     tpm_file="${tpm_dir}/${project_id}-tpm.tsv"
+    pseudobulk_file="${pseudobulk_dir}/${project_id}-pseudobulk.rds"
 
-    # Step 1: Calculate TPM for each project
-    Rscript ${script_dir}/calculate-tpm.R \
+    # Step 1: Calculate bulk TPM for each project
+    if [ ! -f ${tpm_file} ]; then
+      Rscript ${script_dir}/calculate-tpm.R \
         --input_dir "${scpca_dir}/${project_id}" \
         --output_file "${tpm_file}"
+    fi
+    
+    # Step 2: Calculate pseudobulk matrices for each project
+    if [ ! -f ${pseudobulk_file} ]; then
+      Rscript ${script_dir}/calculate-pseudobulk.R \
+        --input_dir "${scpca_dir}/${project_id}" \
+        --output_file "${pseudobulk_file}"
+    fi
 done
