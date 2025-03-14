f73cf067729258988e709d281f24f0ab9687a261
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-03-14 16:05:58 -0400

    Update script to make both 5b and 5c, since there is a decent amount of shared code

--- scripts/Fig5B-C_brain-barplots.R
+++ scripts/Fig5B-C_brain-barplots.R
@@ -8,13 +8,14 @@ library(patchwork)
 options(readr.show_col_types = FALSE)
 
 celltype_plotting_functions <- here::here("scripts", "utils", "consensus-celltype-plotting-functions.R")
-source(celltype_plotting_functions) # imports `stacked_barchart()`
+source(celltype_plotting_functions) # imports `stacked_barchart()`, `create_celltype_summary()` (for 5B), and `create_immune_celltype_summary()` (for 5C)
 
 # Set up paths -----------------------------------------------------------------
 
 # output files 
 pdf_dir <- here::here("figures", "pdfs")
-output_pdf_file <- file.path(pdf_dir, "Fig5B_brain-barchart-all-celltypes.pdf")
+output_5b_pdf_file <- file.path(pdf_dir, "Fig5B_brain-barchart-all-celltypes.pdf")
+output_5c_pdf_file <- file.path(pdf_dir, "Fig5C_brain-barchart-immune.pdf")
 
 # all metadata files 
 sample_info_dir <- here::here("sample-info")
@@ -30,10 +31,23 @@ consensus_results_dir <- file.path(s3_dir, "cell-type-consensus-results")
 # validation groups and marker gene table urls 
 validation_group_url <- "https://raw.githubusercontent.com/AlexsLemonade/OpenScPCA-analysis/refs/heads/main/analyses/cell-type-consensus/references/consensus-validation-groups.tsv"
 
-# define color palette
-color_palette_file <- here::here("palettes", "validation-group-palette.tsv")
-celltype_colors <- readr::read_tsv(color_palette_file) |> 
+# define color palettes for both plots
+color_palette_all_file <- here::here("palettes", "validation-group-palette.tsv")
+celltype_colors_all <- readr::read_tsv(celltype_colors_all_file) |> 
   tibble::deframe()
+color_palette_immune_file <- here::here("palettes", "immune-palette.tsv")
+celltype_colors_immune <- readr::read_tsv(color_palette_immune_file) |> 
+  tibble::deframe()
+
+# get all possible immune cell types
+immune_url <- "https://raw.githubusercontent.com/AlexsLemonade/OpenScPCA-analysis/refs/tags/v0.2.2/analyses/cell-type-consensus/references/consensus-immune-cell-types.tsv"
+all_immune_celltypes <- readr::read_tsv(immune_url) |> 
+  dplyr::pull(consensus_annotation)
+
+# cell types which will be emphasized in 5C plot
+tcell_celltypes <- c("T cell", "mature T cell", "memory T cell", "regulatory T cell", "CD4-positive alpha-beta T cell", "mature alpha-beta T cell")
+myeloid_celltypes <- c("macrophage", "mononuclear phagocyte", "monocyte", "myeloid leukocyte", "neutrophil", "dendritic cell") # we do not include granulocytes since they are only in 1 sample
+
 
 # Prep metadata ----------------------------------------------------------------
 
@@ -62,7 +76,7 @@ sample_df <- readr::read_tsv(sample_metadata_file) |>
 
 sample_ids <- sample_df$scpca_sample_id
 
-# Prep and plot cell types -----------------------------------------------------
+# Define all cell type files -------------------------------------------
 
 # get list of cell type files using sample ids 
 consensus_results_files <- list.files(
@@ -73,6 +87,9 @@ consensus_results_files <- list.files(
 )
 celltype_files <- consensus_results_files[basename(dirname(consensus_results_files)) %in% sample_ids]
 
+
+# Prep and plot 5B -----------------------------------------------------
+
 # prep data frame for plotting
 summary_df <- create_celltype_summary(celltype_files, validation_groups_df) |> 
   # join with sample metadata
@@ -81,8 +98,37 @@ summary_df <- create_celltype_summary(celltype_files, validation_groups_df) |>
   dplyr::filter(subdiagnosis_group %in% c("High-grade glioma", "Low-grade glioma")) 
 
 # make stacked bar chart and facet 
-hgg_lgg_barplot <- stacked_barchart(summary_df, fill_column = "broad_celltype_group", celltype_colors = celltype_colors, facet_variable = "subdiagnosis_group")
+hgg_lgg_barplot <- stacked_barchart(summary_df, fill_column = "broad_celltype_group", celltype_colors = celltype_colors_all, facet_variable = "subdiagnosis_group")
 
 # save plot 
-ggsave(output_pdf_file, plot = hgg_lgg_barplot, width = 20, height = 10)
+ggsave(output_5b_pdf_file, plot = hgg_lgg_barplot, width = 20, height = 10)
 
+
+
+
+# Prep and plot 5C -----------------------------------------------------
+
+
+summary_df <- create_immune_celltype_summary(
+  celltype_files,
+  all_immune_celltypes, 
+  tcell_celltypes, 
+  myeloid_celltypes,
+  n_immune_cell_threshold = 20
+) |>
+  # join with sample metadata
+  dplyr::left_join(sample_df, by = c("project_id" = "scpca_project_id", "sample_id" = "scpca_sample_id")) |> 
+  # only keep HGG and LGG 
+  dplyr::filter(subdiagnosis_group %in% c("High-grade glioma", "Low-grade glioma"))
+
+
+hgg_lgg_immune_barplot <- stacked_barchart(
+  summary_df, 
+  fill_column = "broad_celltype_group", 
+  celltype_colors = celltype_colors_immune, 
+  facet_variable = "subdiagnosis_group", 
+  fill_label = "Immune cell type"
+)
+
+# save plot 
+ggsave(output_5c_pdf_file, plot = hgg_lgg_immune_barplot, width = 20, height = 10)
