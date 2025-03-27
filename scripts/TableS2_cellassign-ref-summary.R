de36e2367070bc66e4d94a801f855e7364e4ac4b
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-03-27 10:32:31 -0400

    Update path to tsv

--- scripts/TableS2_cellassign-ref-summary.R
+++ scripts/TableS2_cellassign-ref-summary.R
@@ -1,4 +1,4 @@
-# This script creates a table showing the compilation of all references 
+# This script creates a table showing the compilation of all references
 # built for CellAssign from PanglaoDB
 # specifically one row per project, with list of diagnoses, ref name, and organ list
 
@@ -14,53 +14,53 @@ sample_metadata_file <- here::here("s3_files", "scpca-sample-metadata.tsv")
 # need this to get list of organs and ref name
 panglao_metadata_file <- here::here("sample-info", "celltype-reference-metadata.tsv")
 
-# project celltype metadata 
+# project celltype metadata
 # need this to get ref name for each project
-project_metadata_file <- here::here("s3_files", "scpca-project-celltype-metadata.tsv")
-# project whitelist 
+project_metadata_file <- here::here("reproduce-figures-analysis", "scpca-project-celltype-metadata.tsv")
+# project whitelist
 project_whitelist_file <- file.path("sample-info", "project-whitelist.txt")
 
-# output table 
+# output table
 table_dir <- here::here("tables")
 fs::dir_create(table_dir)
 output_table_file <- file.path(table_dir, "TableS2_cellassign-ref-summary.tsv")
 
 # Prepare table ----------------------------------------------------------------
 
-# read in all metadata 
+# read in all metadata
 sample_df <- readr::read_tsv(sample_metadata_file)
 project_df <- readr::read_tsv(project_metadata_file)
-# only keep ref name and organs for joining later 
-panglao_df <- readr::read_tsv(panglao_metadata_file) |> 
+# only keep ref name and organs for joining later
+panglao_df <- readr::read_tsv(panglao_metadata_file) |>
   dplyr::select(celltype_ref_name, organs)
 
 # read in project whitelist
 project_whitelist <- readLines(project_whitelist_file)
 
 # filter project df to include only projects with a cellassign ref
-project_df <- project_df |> 
+project_df <- project_df |>
   dplyr::filter(!is.na(cellassign_ref_name),
-                scpca_project_id %in% project_whitelist) |> 
-  # only keep project ID and ref name for joining 
+                scpca_project_id %in% project_whitelist) |>
+  # only keep project ID and ref name for joining
   dplyr::select(scpca_project_id, cellassign_ref_name)
 
-grouped_sample_df <- sample_df |> 
-  dplyr::select(scpca_project_id, diagnosis) |> 
+grouped_sample_df <- sample_df |>
+  dplyr::select(scpca_project_id, diagnosis) |>
   # remove any projects that don't have cell assign refs
-  dplyr::filter(scpca_project_id %in% project_df$scpca_project_id) |> 
-  dplyr::group_by(scpca_project_id) |> 
+  dplyr::filter(scpca_project_id %in% project_df$scpca_project_id) |>
+  dplyr::group_by(scpca_project_id) |>
   # create a list of diagnoses for each project
-  dplyr::summarize(Diagnoses = paste(sort(unique(diagnosis)), collapse = ", ")) |> 
+  dplyr::summarize(Diagnoses = paste(sort(unique(diagnosis)), collapse = ", ")) |>
   # add ref name
-  dplyr::left_join(project_df) |> 
+  dplyr::left_join(project_df) |>
   # add organs
-  dplyr::left_join(panglao_df, by = c("cellassign_ref_name" = "celltype_ref_name")) |> 
+  dplyr::left_join(panglao_df, by = c("cellassign_ref_name" = "celltype_ref_name")) |>
   dplyr::rename(
     "ScPCA Project ID" = "scpca_project_id",
     "ScPCA reference name" = "cellassign_ref_name",
     "PanglaoDB organs included in reference" = "organs"
   )
 
-# export table 
+# export table
 readr::write_tsv(grouped_sample_df, output_table_file)
 
