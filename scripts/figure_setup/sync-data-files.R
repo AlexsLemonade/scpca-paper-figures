150c7146fa88f3aad3a94c8f7b9b6ecf445c7c0a
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-04-03 10:54:04 -0400

    Found a path to fix while here since this s3 path is outdated

--- scripts/figure_setup/sync-data-files.R
+++ scripts/figure_setup/sync-data-files.R
@@ -1,4 +1,4 @@
-# This script is used to download any data needed to generate figures 
+# This script is used to download any data needed to generate figures
 # All files will be saved in a folder named `s3_files`
 
 renv::load()
@@ -65,7 +65,7 @@ sync_call <- glue::glue("aws s3 sync '{celltype_s3_files}' '{celltype_local_file
 system(sync_call)
 
 # sync merged object results for SCPCP000003 -----------------------------------
-merged_s3_dir <- "s3://nextflow-ccdl-results/scpca/processed/results/merged/SCPCP000003"
+merged_s3_dir <- "s3://nextflow-ccdl-results/scpca-prod/results/SCPCP000003/merged/"
 merged_local_dir <- here::here("s3_files", "SCPCP000003")
 fs::dir_create(merged_local_dir)
 
