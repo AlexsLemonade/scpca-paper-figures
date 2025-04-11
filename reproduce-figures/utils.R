8fba9d8ff89406c6da74c99e01567638fb7d790a
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-04-11 09:36:00 -0400

    Add a function to prepare the library metadata from several different input metadata files, but this currently still uses the portal-wide download

--- reproduce-figures/utils.R
+++ reproduce-figures/utils.R
@@ -75,3 +75,99 @@ prepare_gene_expression_tsv <- function(sce, marker_genes, output_tsv) {
   # export the marker gene expression tsv
   readr::write_tsv(gene_exp_df, output_tsv)
 }
+
+
+
+#' Prepare and export library metadata file
+#'
+#' @param portal_metadata Data frame of portal-wide metadata
+#' @param bulk_metadata_dir Directory with bulk metadata TSV files
+#' @param project_metadata_dir Directory with project-specific metadata TSV files
+#' @param output_tsv Path to output TSV file
+#'
+prepare_library_metadata <- function(
+    portal_metadata, 
+    bulk_metadata_dir, 
+    project_metadata_dir, 
+    output_tsv) {
+  
+  portal_metadata <- portal_metadata |>
+    # Select columns of interest
+    dplyr::select(
+      scpca_project_id,
+      scpca_sample_id,
+      scpca_library_id,
+      seq_unit, 
+      technology
+    ) |>
+    # Group multiplexed sample ids back together
+    dplyr::group_by(scpca_project_id, scpca_library_id, seq_unit, technology) |>
+    dplyr::summarize(scpca_sample_id = paste(scpca_sample_id, collapse = ";"))
+    
+  
+  # Define bulk and project metadata files
+  bulk_files <- list.files(
+    path = bulk_metadata_dir, 
+    pattern = "*_bulk_metadata\\.tsv", 
+    full.names = TRUE
+  )
+  project_files <- list.files(
+    path = project_metadata_dir, 
+    pattern = "*_single_cell_metadata\\.tsv", 
+    full.names = TRUE
+  ) |>
+    # we need project id identifiers for this one
+    purrr::set_names(\(x) {stringr::str_split_i(basename(x), "_", 1)})
+  
+  # Parse bulk metadata
+  bulk_metadata <- bulk_files |>
+    purrr::map(readr::read_tsv) |>
+    purrr::list_rbind() |>
+    dplyr::select(
+      scpca_project_id = project_id, 
+      scpca_sample_id = sample_id, 
+      scpca_library_id = library_id, 
+      seq_unit, 
+      technology
+    ) 
+  
+  # Parse project metadata to get rows for CITE-seq & cellhash libraries
+  multimodal_metadata <- project_files |>
+    purrr::imap(
+      \(file, project_id) {
+        metadata_df <- readr::read_tsv(file) 
+        
+        # Get information for cellhash and then CITE-seq projects
+        if (project_id == "SCPCP000009") {
+          metadata_df <- metadata_df |>
+            dplyr::group_by(scpca_project_id, scpca_library_id, seq_unit, technology) |>
+            dplyr::summarize(scpca_sample_id = paste(scpca_sample_id, collapse = ";")) |>
+            dplyr::mutate(technology = "cellhash")
+        } else {
+          metadata_df <- metadata_df |>
+            dplyr::filter(!is.na(adt_filtering_method)) |>
+            dplyr::mutate(technology = "CITE-seq")
+        } 
+        
+        metadata_df <- metadata_df |>
+          dplyr::select(
+            scpca_project_id,
+            scpca_sample_id,
+            scpca_library_id,
+            seq_unit,
+            technology
+          )
+        
+        return(metadata_df)
+      }
+    ) |>
+    purrr::list_rbind() |>
+    dplyr::ungroup()
+  
+  # Combine data frames and export
+  portal_metadata |>
+    dplyr::bind_rows(bulk_metadata) |>
+    dplyr::bind_rows(multimodal_metadata) |>
+    dplyr::arrange(scpca_project_id) |>
+    readr::write_tsv(output_tsv)
+}
