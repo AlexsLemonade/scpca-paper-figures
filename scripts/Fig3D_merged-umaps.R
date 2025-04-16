a0efa94aef75f900f44cf0b0af9d0b0b337f9b00
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-04-16 16:15:11 -0400

    Update script to make plot directly from processed files

--- scripts/Fig3D_merged-umaps.R
+++ scripts/Fig3D_merged-umaps.R
@@ -1,11 +1,24 @@
-# This script is used to generate a small version of the merged UMAP for Figure 3
+# This script is used to generate a small version of the merged UMAP for Figure 3D
+# By default, it used 4 threads.
 
 # load project
 renv::load()
 
 library(SingleCellExperiment)
-library(dplyr)
 library(ggplot2)
+library(optparse)
+
+
+option_list <- list(
+  make_option(
+    "--threads",
+    default = 4, 
+    type = "integer",
+    help = "Number of threads to use"
+  )
+)
+opt <- parse_args(OptionParser(option_list = option_list))
+
 
 # Set default ggplot theme
 theme_set(
@@ -25,14 +38,34 @@ theme_set(
 
 # Set up -----------------------------------------------------------------------
 
+# set up multiprocessing params
+if (opt$threads > 1) {
+  bp_param <- BiocParallel::MulticoreParam(opt$threads)
+} else {
+  bp_param <- BiocParallel::SerialParam()
+}
+
+libraries_to_plot <- c("SCPCL000050",
+                       "SCPCL000697",
+                       "SCPCL000698",
+                       "SCPCL000705")
+
 # folder where any local data lives
 local_results_dir <- here::here("s3_files", "SCPCP000003")
 
-# define file path to merged object 
-merged_sce_file <- file.path(local_results_dir, "SCPCP000003_merged.rds")
+# define file paths to sce files 
+rds_files <- list.files(
+  path = local_results_dir, 
+  pattern = "_processed.rds", 
+  full.names = TRUE
+) |>
+  purrr::set_names(\(x) {stringr::str_split_i(basename(x), "_", 1)})
 
-# read in merged sce objects 
-merged_sce <- readr::read_rds(merged_sce_file)
+# Ensure we have the correct libraries
+rds_files <- rds_files[libraries_to_plot]
+stopifnot(
+  "Libraries needed to make figure 3D are not all present" = length(setdiff(libraries_to_plot, names(rds_files))) == 0
+)
 
 # define output file paths 
 png_dir <- here::here("figures", "pngs") 
@@ -42,31 +75,71 @@ pdf_dir <- here::here("figures", "pdfs")
 output_pdf_file <- file.path(pdf_dir, "Fig3D_merged-umaps.pdf")
 
 
-# Prep for plotting ------------------------------------------------------------
+# Read, merge, and process objects ------------------------------------------------
 
-libraries_to_plot <- c("SCPCL000050",
-                       "SCPCL000697",
-                       "SCPCL000698",
-                       "SCPCL000705")
+# Read in files and merge into a single object
+sce_list <- rds_files |>
+  purrr::map(
+    \(rds_file) {
+      sce <- readr::read_rds(rds_file)
+      
+      # Add library id to coldata before merging
+      sce$library_id <- metadata(sce)$library_id
+      
+      return(sce)
+  })
+# note that this will produce warnings from rowData merging, but these are ok; this is not relevant to the plot
+merged_sce <- do.call(combineCols, unname(sce_list)) 
 
-# extract coldata as data frame to use to create tables and UMAPs
-coldata_df <- scuttle::makePerCellDF(merged_sce, use.dimred = "UMAP") |>
-  dplyr::rename(
-    UMAP1 = UMAP.1,
-    UMAP2 = UMAP.2
-  ) |> 
-  dplyr::filter(library_id %in% libraries_to_plot)
+# clean up for space
+rm(sce_list)
+  
+# Perform batch-aware PCA and calculate UMAP
+# Code adapted from `scpca-nf`: https://github.com/AlexsLemonade/scpca-nf/blob/5cbacbf5be11e8b56c6595bc434eb4e85a483865/bin/merge_sces.R#L321
 
-# UMAPs ------------------------------------------------------------------------
+batch_column <- merged_sce$library_id
+
+# model gene variance & identify subset of variable genes
+hvg_list <- scran::modelGeneVar(
+  merged_sce, 
+  block = batch_column, 
+  BPPARAM = bp_param
+  ) |>
+  scran::getTopHVGs(n = 2000)
+
+# perform multi batch PCA on merged object
+multi_pca <- batchelor::multiBatchPCA(
+  merged_sce,
+  subset.row = hvg_list,
+  batch = batch_column,
+  preserve.single = TRUE, 
+  BPPARAM = bp_param
+)
+
+# add PCA results to merged SCE object
+reducedDim(merged_sce, "PCA") <- multi_pca[[1]]
+
+# add UMAP
+merged_sce <- scater::runUMAP(
+  merged_sce,
+  dimred = "PCA"
+)
+
+# Plot the UMAP -------------
+umap_df <- data.frame(
+  library_id = batch_column, 
+  UMAP1 = reducedDim(merged_sce, "UMAP")[,1],
+  UMAP2 = reducedDim(merged_sce, "UMAP")[,2]
+)
 
 umap_plot <- ggplot(
-  coldata_df,
+  umap_df,
   aes(x = UMAP1, y = UMAP2, color = library_id)
 ) +
   # set points for all "other" points
   geom_point(
     data = dplyr::select(
-      coldata_df, -c("library_id")
+      umap_df, -c("library_id")
     ),
     color = "gray80",
     alpha = 0.5,
