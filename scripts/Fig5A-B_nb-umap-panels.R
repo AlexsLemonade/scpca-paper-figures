a61593bda9dc37e55df6a6335b75dd36850b0178
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2025-12-08 16:30:51 -0500

    comment

--- scripts/Fig5A-B_nb-umap-panels.R
+++ scripts/Fig5A-B_nb-umap-panels.R
@@ -1,4 +1,4 @@
-# This script generates UMAP panels 5A and 5B 
+# This script generates UMAP panels 5A and 5B
 
 # load project
 renv::load()
@@ -27,7 +27,12 @@ merged_sce_file <- file.path(data_dir, glue::glue("{project_id}_merged.rds"))
 merged_sce_df <- readr::read_rds(merged_sce_file) |>
   # convert to data frame immediately for space
   scuttle::makePerCellDF(
-    use.coldata = c("cell_id", "library_id", "infercnv_total_cnv", "openscpca_celltype_annotation"),
+    use.coldata = c(
+      "cell_id",
+      "library_id",
+      "infercnv_total_cnv",
+      "openscpca_celltype_annotation"
+    ),
     use.dimred = "UMAP"
   ) |>
   # remove rownames
@@ -37,71 +42,77 @@ merged_sce_df <- readr::read_rds(merged_sce_file) |>
 output_panel_a_file <- here::here("figures", "pdfs", "Fig5A-umap-celltypes.pdf")
 output_panel_b_file <- here::here("figures", "pdfs", "Fig5B-umap-infercnv.pdf")
 
-  
+
 # Prepare data for plotting ----------------------------------------------------
 # Collapse cell types
 merged_sce_df <- merged_sce_df |>
   dplyr::mutate(
     cell_category = dplyr::case_when(
-      openscpca_celltype_annotation == "Neuroendocrine" ~ "malignant", 
-      openscpca_celltype_annotation %in% c("Unknown", "openscpca-excluded") ~ "unknown",
-      openscpca_celltype_annotation %in% c("Fibroblast", "Endothelial", "Schwann") ~ openscpca_celltype_annotation,
+      openscpca_celltype_annotation == "Neuroendocrine" ~ "malignant",
+      openscpca_celltype_annotation %in%
+        c("Unknown", "openscpca-excluded") ~ "unknown",
+      openscpca_celltype_annotation %in%
+        c(
+          "Fibroblast",
+          "Endothelial",
+          "Schwann"
+        ) ~ openscpca_celltype_annotation,
       # everything else is a type of immune cell
       .default = "immune cell"
     ),
     # tack on "cell" in a few spots
     cell_category = ifelse(
-      cell_category %in% c("Endothelial", "Schwann", "malignant"), 
-      glue::glue("{cell_category} cell"), 
+      cell_category %in% c("Endothelial", "Schwann", "malignant"),
+      glue::glue("{cell_category} cell"),
       cell_category
     ),
     # make everything lowercase, consistent with other figures
     cell_category = stringr::str_to_lower(cell_category),
     # determine factor order based on frequency, with Unknown last
-    cell_category = forcats::fct_infreq(cell_category), 
+    cell_category = forcats::fct_infreq(cell_category),
     cell_category = forcats::fct_relevel(cell_category, "unknown", after = Inf)
   )
 
-# TODO this needs to be in its own file read in as input
+# define nbatlas condensed palette
 cell_palette <- c(
   "malignant cell" = "rosybrown4",
   "immune cell" = "darkgreen",
   "endothelial cell" = "#FC1CBF",
-  "fibroblast" = "#B10DA1", 
+  "fibroblast" = "#B10DA1",
   "schwann cell" = "chocolate1",
   "unknown" = "gray70" # darker than validation palette since we have some standalone unknown clumps
 )
 
 # Panel A ------------------
-celltype_umap <- ggplot(merged_sce_df) + 
-  aes(x = UMAP.1, y = UMAP.2, color = cell_category) + 
-  geom_point(alpha = 0.25, size = 0.25) + 
-  scale_color_manual(values = cell_palette) + 
+celltype_umap <- ggplot(merged_sce_df) +
+  aes(x = UMAP.1, y = UMAP.2, color = cell_category) +
+  geom_point(alpha = 0.25, size = 0.25) +
+  scale_color_manual(values = cell_palette) +
   guides(
     color = guide_legend(override.aes = list(alpha = 1, size = 1.5))
   ) +
   labs(
-    x = "UMAP1", 
-    y = "UMAP2", 
+    x = "UMAP1",
+    y = "UMAP2",
     color = "OpenScPCA broad\ncell type annotation"
   ) +
   theme(
-    legend.position = "bottom", 
+    legend.position = "bottom",
     legend.direction = "horizontal"
-  ) 
-  
+  )
+
 
 # Panel B -----------------------------
-infercnv_umap <- ggplot(merged_sce_df) + 
-aes(x = UMAP.1, y = UMAP.2, color = infercnv_total_cnv) + 
-  geom_point(alpha = 0.25, size = 0.25) + 
+infercnv_umap <- ggplot(merged_sce_df) +
+  aes(x = UMAP.1, y = UMAP.2, color = infercnv_total_cnv) +
+  geom_point(alpha = 0.25, size = 0.25) +
   scale_color_viridis_c(na.value = "grey70") +
   labs(
-    x = "UMAP1", 
-    y = "UMAP2", 
+    x = "UMAP1",
+    y = "UMAP2",
     color = "Total CNV"
   ) +
-  theme(legend.position = "bottom") 
+  theme(legend.position = "bottom")
 
 
 # Export --------------------------------
